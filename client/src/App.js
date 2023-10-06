96f0bf3eb6b6607848f99c48b14abb9715273251
Ndubuisi William <princewill835@gmail.com>
2023-10-06 14:42:40 -0700

    netlify page error officially added

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import DepositForm from "./pages/DepositPage.js";
 import TransferForm from "./pages/TransferPage.js";
 
 const httpLink = createHttpLink({
-	uri: "https://bankbankend2.onrender.com/graphql",
+	uri: " https://bankbankend2.onrender.com/graphql",
 });
 const authLink = setContext((_, { headers }) => {
 	// Get the authentication token from wherever you've stored it
